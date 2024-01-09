764895308aab81d0cd7bca82d74351ee3af80966
nsanzimfura-eric <erickykress1@gmail.com>
2024-01-09 22:07:22 +0200

    Add Metropolis fonts and mixins, and variable4s

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import styles from "./about.module.scss";
+
+export default function About() {
+    return (
+        <div className={styles.about}>About</div>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/main.scss";`,
+      },
+    },
+  },
 });
