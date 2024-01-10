74e08c699a11692ac0654ffadd66971802f7a8d1
nsanzimfura-eric <erickykress1@gmail.com>
2024-01-10 18:27:39 +0200

    Finish aboutUs and Missions design and responsiveness

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -1,5 +1,4 @@
 import styles from "./AboutUs.module.scss";
-
 interface Prop {
     reverseColumns: boolean;
     data: {
@@ -11,17 +10,21 @@ interface Prop {
     };
 }
 const AboutUs = (props: Prop) => {
+
     const { reverseColumns, data } = props;
 
     return (
-        <div className={`${styles.aboutUs} container d-flex flex-column pt-5`}>
-            <div className={`wrapperBox w-100 d-flex flex-column flex-lg-row m-0 ${reverseColumns ? "reverseColumns" : ""}`}>
-                <img src={data.image} alt="Burger APP" className="image-burger" />
-                <div className="aboutBox">
-                    <h3>{data.title}</h3>
-                    <h2>{data.bigTitle}</h2>
-                    <p>{data.details}</p>
+        <div className={`${styles.aboutUs} w-100 p-0 m-0 d-flex pt-5`}>
+            <div className="mt-5 pt-5 container containerWrapper">
+                <div className={`wrapperBox d-flex flex-column-reverse  flex-lg-row m-0 ${reverseColumns ? "reverseColumns" : ""}`}>
+                    <img src={data.image} alt="Burger APP" className="image-burger" />
+                    <div className="aboutBox">
+                        <h3>{data.title}</h3>
+                        <h2>{data.bigTitle}</h2>
+                        <p>{data.details}</p>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,16 +1,35 @@
+import { useState, useEffect } from "react";
 import NavbarComponent from "../../components/Navbar/Navbar";
 import AboutUs from "../../components/about/AboutUs";
 import Hero from "../../components/hero/Hero";
 import styles from "./about.module.scss";
 import { aboutUsData, missionData } from "./demoData"
+import useWindowSize from "../../hooks/useWindowSize";
+import { mediaDevices } from "../../utils/constants";
 
 export default function About() {
+    const [reverseColumns, setReverseColumns] = useState(true);
+    const [reverseColumns2, setReverseColumns2] = useState(false);
+
+    const { width } = useWindowSize();
+
+    useEffect(() => {
+        if (width <= mediaDevices.lg) {
+            setReverseColumns(false);
+            setReverseColumns2(false);
+        } else {
+            setReverseColumns(true);
+            setReverseColumns2(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [width])
+
     return (
         <div className={`${styles.about} container-fluid m-0 p-0 bg-pale w-100`}>
             <NavbarComponent />
             <Hero />
-            <AboutUs reverseColumns={false} data={aboutUsData} />
-            <AboutUs reverseColumns={true} data={missionData} />
+            <AboutUs reverseColumns={reverseColumns2} data={aboutUsData} />
+            <AboutUs reverseColumns={reverseColumns} data={missionData} />
         </div>
     )
 }
